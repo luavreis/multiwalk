1c3f1f288d67da98c04e6927f4c31643f962442f
Lucas V. R <lvr@s-viva.xyz>
2023-01-23 19:33:15 -0300

    writer monad benchmarks

--- test/Benchmark.hs
+++ test/Benchmark.hs
@@ -20,7 +20,11 @@ import Test.Tasty.HUnit
 import Text.Pandoc.Builder (Blocks, toList)
 import Text.Pandoc.Definition
 import Text.Pandoc.Generic (queryWith)
+import qualified Control.Monad.Trans.Writer.Lazy as LW
+import qualified Control.Monad.Trans.Writer.Strict as SW
+import qualified Control.Monad.Trans.Writer.Strict as CPSW
 import qualified Text.Pandoc.Walk as PW
+import Data.Functor (($>))
 
 data PTag
 
@@ -54,6 +58,36 @@ prepEnv = do
   text <- decodeUtf8 <$> B.readFile "test/text.md"
   Right (Cm b :: Cm () Blocks) <- pure $ commonmark "test/text.md" text
   evaluate $ force $ toList b
+
+multiLW :: [Block] -> [Text]
+multiLW = foldMap (LW.execWriter . w)
+  where
+    w = buildMultiW @PTag $ \f list ->
+      let blks x@(CodeBlock _ c) = LW.tell [c] $> x
+          blks x = f x
+          inls x@(Code _ c) = LW.tell [c] $> x
+          inls x = f x
+       in list .> blks .> inls
+
+multiSW :: [Block] -> [Text]
+multiSW = foldMap (SW.execWriter . w)
+  where
+    w = buildMultiW @PTag $ \f list ->
+      let blks x@(CodeBlock _ c) = SW.tell [c] $> x
+          blks x = f x
+          inls x@(Code _ c) = SW.tell [c] $> x
+          inls x = f x
+       in list .> blks .> inls
+
+multiCPSW :: [Block] -> [Text]
+multiCPSW = foldMap (CPSW.execWriter . w)
+  where
+    w = buildMultiW @PTag $ \f list ->
+      let blks x@(CodeBlock _ c) = CPSW.tell [c] $> x
+          blks x = f x
+          inls x@(Code _ c) = CPSW.tell [c] $> x
+          inls x = f x
+       in list .> blks .> inls
 
 multi :: [Block] -> [Text]
 multi = foldMap $
@@ -93,6 +127,9 @@ main =
     [ bgroup
         "query"
         [ env prepEnv $ bench "multiwalk" . nf multi,
+          env prepEnv $ bench "mw lw" . nf multiLW,
+          env prepEnv $ bench "mw sw" . nf multiSW,
+          env prepEnv $ bench "mw cpsw" . nf multiCPSW,
           env prepEnv $ bench "syb" . nf gene,
           env prepEnv $ bench "pandoc.walk" . nf wal,
           env prepEnv $ \blocks ->
