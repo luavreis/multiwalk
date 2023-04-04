5620585361941623557394e50a3efe4ce439cb98
Lucas V. R <lvr@s-viva.xyz>
2023-04-04 16:33:22 -0300

    Constructor and field specifiers

--- src/Control/MultiWalk/HasSub.hs
+++ src/Control/MultiWalk/HasSub.hs
@@ -7,11 +7,18 @@ import Control.Applicative (liftA2)
 import Data.Kind (Constraint, Type)
 import Data.Proxy (Proxy (..))
 import GHC.Generics
+import GHC.TypeLits
 
-data SubSpec = SubSpec Type Type
+data SubSpec = SubSpec SelSpec Type Type
+
+data SelSpec
+  = NoSel
+  | ConsSel Symbol
+  | FieldSel Symbol
+  | ConsFieldSel Symbol Symbol
 
 type family SpecCarrier (t :: SubSpec) where
-  SpecCarrier ('SubSpec a _) = a
+  SpecCarrier ('SubSpec _ a _) = a
 
 class HasSub tag (ls :: [SubSpec]) t where
   modSub ::
@@ -35,9 +42,9 @@ instance HasSub tag '[] t where
 
 data GSubTag
 
-instance (Generic t, HasSub' (l : ls) (Rep t)) => HasSub GSubTag (l : ls) t where
-  modSub p f = fmap to . modSub' @(l : ls) p f . from
-  getSub p f = getSub' @(l : ls) p f . from
+instance (Generic t, HasSub' (l : ls) 'Nothing 'Nothing (Rep t)) => HasSub GSubTag (l : ls) t where
+  modSub p f = fmap to . modSub' @(l : ls) @'Nothing @'Nothing p f . from
+  getSub p f = getSub' @(l : ls) @'Nothing @'Nothing p f . from
 
 -- Generic code
 
@@ -45,7 +52,7 @@ type family All (p :: k -> Constraint) (as :: [k]) :: Constraint where
   All p '[] = ()
   All p (a ': as) = (p a, All p as)
 
-class HasSub' (ls :: [SubSpec]) (t :: Type -> Type) where
+class HasSub' (ls :: [SubSpec]) (cname :: Maybe Symbol) (sname :: Maybe Symbol) (t :: Type -> Type) where
   modSub' ::
     forall c m p.
     (Applicative m, All c ls) =>
@@ -61,32 +68,71 @@ class HasSub' (ls :: [SubSpec]) (t :: Type -> Type) where
     t p ->
     m
 
-instance HasSub' ('SubSpec tc tt : ls) (K1 j tc) where
-  modSub' _ f (K1 x) = K1 <$> f (Proxy @('SubSpec tc tt)) x
-  getSub' _ f (K1 x) = f (Proxy @('SubSpec tc tt)) x
+instance HasSub' ('SubSpec 'NoSel t tt : ls) _a _b (K1 _c t) where
+  modSub' _ f (K1 x) = K1 <$> f (Proxy @('SubSpec 'NoSel t tt)) x
+  getSub' _ f (K1 x) = f (Proxy @('SubSpec 'NoSel t tt)) x
 
-instance {-# OVERLAPPABLE #-} HasSub' ls (K1 j s) => HasSub' (l : ls) (K1 j s) where
-  modSub' = modSub' @ls
-  getSub' = getSub' @ls
+instance HasSub' ('SubSpec ('FieldSel s) t tt : ls) _a ('Just s) (K1 _c t) where
+  modSub' _ f (K1 x) = K1 <$> f (Proxy @('SubSpec ('FieldSel s) t tt)) x
+  getSub' _ f (K1 x) = f (Proxy @('SubSpec ('FieldSel s) t tt)) x
 
-instance HasSub' '[] (K1 j t) where
+instance HasSub' ('SubSpec ('ConsSel s) t tt : ls) ('Just s) _b (K1 _c t) where
+  modSub' _ f (K1 x) = K1 <$> f (Proxy @('SubSpec ('ConsSel s) t tt)) x
+  getSub' _ f (K1 x) = f (Proxy @('SubSpec ('ConsSel s) t tt)) x
+
+instance HasSub' ('SubSpec ('ConsFieldSel s1 s2) t tt : ls) ('Just s1) ('Just s2) (K1 _c t) where
+  modSub' _ f (K1 x) = K1 <$> f (Proxy @('SubSpec ('ConsFieldSel s1 s2) t tt)) x
+  getSub' _ f (K1 x) = f (Proxy @('SubSpec ('ConsFieldSel s1 s2) t tt)) x
+
+instance {-# OVERLAPPABLE #-} HasSub' ls a b (K1 j s) => HasSub' (l : ls) a b (K1 j s) where
+  modSub' = modSub' @ls @a @b
+  getSub' = getSub' @ls @a @b
+
+instance HasSub' '[] _a _b (K1 _c _d) where
   modSub' _ _ = pure
   getSub' _ _ = mempty
 
-instance (HasSub' s x, HasSub' s y) => HasSub' s (x :*: y) where
-  modSub' p f (x :*: y) = liftA2 (:*:) (modSub' @s p f x) (modSub' @s p f y)
-  getSub' p f (x :*: y) = getSub' @s p f x <> getSub' @s p f y
+instance
+  ( HasSub' s a 'Nothing x
+  , HasSub' s a 'Nothing y
+  ) =>
+  HasSub' s a 'Nothing (x :*: y)
+  where
+  modSub' p f (x :*: y) = liftA2 (:*:) (modSub' @s @a @'Nothing p f x) (modSub' @s @a @'Nothing p f y)
+  getSub' p f (x :*: y) = getSub' @s @a @'Nothing p f x <> getSub' @s @a @'Nothing p f y
 
-instance HasSub' s U1 where
+instance HasSub' _a _b _c U1 where
   modSub' _ _ = pure
   getSub' _ _ = mempty
 
-instance (HasSub' s x, HasSub' s y) => HasSub' s (x :+: y) where
-  modSub' p f (L1 x) = L1 <$> modSub' @s p f x
-  modSub' p f (R1 x) = R1 <$> modSub' @s p f x
-  getSub' p f (L1 x) = getSub' @s p f x
-  getSub' p f (R1 x) = getSub' @s p f x
+instance
+  ( HasSub' s 'Nothing 'Nothing x
+  , HasSub' s 'Nothing 'Nothing y
+  ) =>
+  HasSub' s 'Nothing 'Nothing (x :+: y)
+  where
+  modSub' p f (L1 x) = L1 <$> modSub' @s @'Nothing @'Nothing p f x
+  modSub' p f (R1 x) = R1 <$> modSub' @s @'Nothing @'Nothing p f x
+  getSub' p f (L1 x) = getSub' @s @'Nothing @'Nothing p f x
+  getSub' p f (R1 x) = getSub' @s @'Nothing @'Nothing p f x
 
-instance (HasSub' s x) => HasSub' s (M1 a b x) where
-  modSub' p f (M1 x) = M1 <$> modSub' @s p f x
-  getSub' p f (M1 x) = getSub' @s p f x
+instance
+  HasSub' ls a ('Just s) x =>
+  HasSub' ls a 'Nothing (S1 ('MetaSel ('Just s) _a _b _c) x)
+  where
+  modSub' p f (M1 x) = M1 <$> modSub' @ls @a @('Just s) p f x
+  getSub' p f (M1 x) = getSub' @ls @a @('Just s) p f x
+
+instance
+  HasSub' ls ('Just s) 'Nothing x =>
+  HasSub' ls 'Nothing 'Nothing (C1 ('MetaCons s _a _b) x)
+  where
+  modSub' p f (M1 x) = M1 <$> modSub' @ls @('Just s) @'Nothing p f x
+  getSub' p f (M1 x) = getSub' @ls @('Just s) @'Nothing p f x
+
+instance
+  HasSub' ls 'Nothing 'Nothing x =>
+  HasSub' ls 'Nothing 'Nothing (D1 _a x)
+  where
+  modSub' p f (M1 x) = M1 <$> modSub' @ls @'Nothing @'Nothing p f x
+  getSub' p f (M1 x) = getSub' @ls @'Nothing @'Nothing p f x
